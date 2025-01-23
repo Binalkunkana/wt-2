25bdfb1aeb502c2c34091ca9f49acf5079d03a49
Binalkunkana <24010101655@darshan.ac.in>
2025-01-23 10:59:55 +0530

    "Extra Program"

---
+++ Extra-Task/First50Number.js
@@ -0,0 +1,6 @@
+var sum=1
+var n=50
+for(var i=1;i<=n;i++){
+    console.log(sum)
+    sum=sum+3
+}
---
+++ Extra-Task/class.js
@@ -0,0 +1,8 @@
+class person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+}
+const empl=new person("binal",20)
+console.log(empl)
